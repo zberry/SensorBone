1a5697d393a3655ce9c78e825de763c13ebfcb2e
Zach Berry <zacharyberry@gmail.com>
2017-05-15 07:52:28 -0500

    Added IPHostname resolver script

--- sensors/src/sensors.cpp
+++ sensors/src/sensors.cpp
@@ -9,7 +9,28 @@
 #include <iostream>
 using namespace std;
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <cstring>
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+        setenv("LANG","C",1);
+        FILE * fp = popen("/sbin/ifconfig", "r");
+        if (fp) {
+                char *p=NULL, *e; size_t n;
+                while ((getline(&p, &n, fp) > 0) && p) {
+                   if (p = strstr(p, "inet ")) {
+                        p+=5;
+                        if (p = strchr(p, ':')) {
+                            ++p;
+                            if (e = strchr(p, ' ')) {
+                                 *e='\0';
+                                 printf("%s\n", p);
+                            }
+                        }
+                   }
+              }
+        }
+        pclose(fp);
+        return 0;
 }
